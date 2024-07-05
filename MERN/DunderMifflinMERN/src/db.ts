9af0a32d7ea7fbeaa48c760660b82c14cefa6524
HurBC <fcobreque1204@gmail.com>
2024-07-05 00:36:23 -0400

    🏗️ Change db conection to a MongoDB Atlas db

--- MERN/DunderMifflinMERN/src/db.ts
+++ MERN/DunderMifflinMERN/src/db.ts
@@ -3,13 +3,19 @@ import { Db, MongoClient } from "mongodb";
 let db: Db;
 
 export const connectDB = async () => {
-	const client = new MongoClient(process.env.MONGODB_URI!);
+  console.log("[MONGODB] Connecting to MongoDB");
 
-	await client.connect();
+  try {
+    const client = new MongoClient(process.env.MONGODB_URI!);
+    
+    await client.connect();
+    
+    db = await client.db(process.env.MONGO_DATABASE);
 
-	db = client.db(process.env.MONGODB_DB);
-
-	console.log("[MONGODB] Connected to MongoDB", db.databaseName);
-};
+    console.log("[MONGODB] Connected to MongoDB", db.databaseName);
+  } catch(error) {
+		console.log(error);
+	}
+}
 
 export const getDB = () => db;
